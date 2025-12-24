1f31c66d1a6f4b51f76899e9f066d3321b56c01e
MuzammilKhatri9884 <muzzamil.khatri.95@gmail.com>
2025-12-24 16:32:13 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-var h1E1 = document.getElementById("Heading")
-var Image1 = document.getElementById("Picture");
+function playgame(){
+var coine = 1000;
 
-function onHover(){
-    h1E1.innerText = "Bulb is On"
-    Image1.title = "Bulb is On"
-    Image1.src = "./Image/Bulb2.jpg"
+for (var i = 1; coine > 0; i++) {
+
+    var entryLuckNumber = +prompt("Enter Lucky Number (1 to 6)");
+    var entryAmt = +prompt("Enter Betting Amount");
+    var compNumber = Math.floor(Math.random() * 6) + 1;
+
+    console.log("Current Balance : " + coine);
+    console.log("Your Lucky Number : " + entryLuckNumber);
+    console.log("Betting Amount : " + entryAmt);
+    console.log("Computer Number : " + compNumber);
+
+    if (entryAmt > coine) {
+        console.log("You don't have sufficient balance. Try smaller bet.");
+    }
+    else {
+        if (entryLuckNumber === compNumber) {
+            coine = coine + entryAmt;
+            console.log("You WON Rs: " + entryAmt);
+        }
+        else {
+            coine = coine - entryAmt;
+            console.log("You LOST Rs: " + entryAmt);
+        }
+
+        console.log("Remaining Balance: " + coine);
+    }
 }
-function offHover(){
-    h1E1.innerText = "Bulb is Off"
-    Image1.title = "Bulb is Off"
-    Image1.src = "./Image/Bulb.jpg"
+
+console.log("Game Over! Your balance is 0");
 }
